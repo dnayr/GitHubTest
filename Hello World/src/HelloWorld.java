9a736fdc1d77dd3876ffbbcbf15834d34e8eb630
DNayr <drahcir_nayr@hotmail.com>
2013-11-26 14:49:12 -0500

    New project HelloWorld
    New project for git and general testing...
    
    Signed-off-by: DNayr <drahcir_nayr@hotmail.com>

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] arguments) {
+		float x = 9;
+		float y = 5;
+		int z = (int)(x / y);
+		System.out.print("z=" + z);
+		System.out.print("\nx=" + x);
+		System.out.print("\ny=" + y);
+		switch (z) {
+			case 1:
+				x = x + 2;System.out.print("\ncase1 x=" + x);
+			case 2:
+				x = x + 3;System.out.print("\ncase2 x=" + x);
+			default:
+				x = x + 1;
+		}
+		System.out.println("\nValue of x: " + x);
+	}
+}
